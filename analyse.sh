5269ce4a6583f715cde2c62a1ca80ce23a5cc710
Graham Ashton <graham@effectif.com>
2013-11-25 11:43:29 +0000

    Really produce row of output on days where no work done

--- analyse.sh
+++ analyse.sh
@@ -46,14 +46,20 @@ daily_hours_during_client_job()
     read -r -d '' sql <<EOF
         COPY
            (SELECT DATE(days.timestamp), customer, sum(hours)
-              FROM (
-                  SELECT generate_series(date '$(first_day "$CUSTOMER")',
-                                         date '$(last_day "$CUSTOMER")',
-                                         '1 day'
-              ) AS timestamp) days
-   FULL OUTER JOIN logs ON DATE(timestamp) = logs.day
-          GROUP BY DATE(days.timestamp), customer
-          HAVING customer = '$customer' OR sum(hours) IS NULL
+              FROM
+                  (
+                    SELECT generate_series(date '$(first_day "$CUSTOMER")',
+                                           date '$(last_day "$CUSTOMER")',
+                                           '1 day') AS timestamp
+                  ) days
+              LEFT OUTER JOIN
+                  (
+                    SELECT day, customer, hours FROM logs
+                     WHERE customer = '$customer'
+                        OR project IN ($MY_BOOTSTRAPPED_PROJECTS)
+                  ) customer_logs
+                ON DATE(timestamp) = customer_logs.day
+          GROUP BY DATE(days.timestamp), customer_logs.customer
           ORDER BY DATE(days.timestamp) ASC
           )
         TO '$OUTPUT/$basename-time-entries.csv' WITH csv DELIMITER ','
